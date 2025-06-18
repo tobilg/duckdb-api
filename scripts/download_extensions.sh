fa6ee3cae3b9f02bf4bd5ca01a9abd0695980845
tobilg <tobilg@gmail.com>
2025-06-18 21:26:04 +0200

    Update dependencies & DuckDB version to v1.3.1

--- scripts/download_extensions.sh
+++ scripts/download_extensions.sh
@@ -4,11 +4,13 @@ rm -rf $PWD/extensions
 mkdir -p $PWD/extensions
 
 # Download extensions
-curl https://extensions.duckdb.org/v1.3.0/linux_amd64/httpfs.duckdb_extension.gz --output $PWD/extensions/httpfs.duckdb_extension.gz
-curl https://extensions.duckdb.org/v1.3.0/linux_amd64/iceberg.duckdb_extension.gz --output $PWD/extensions/iceberg.duckdb_extension.gz
-curl https://community-extensions.duckdb.org/v1.3.0/linux_amd64/nanoarrow.duckdb_extension.gz -o $PWD/extensions/nanoarrow.duckdb_extension.gz
+curl https://extensions.duckdb.org/v1.3.1/linux_amd64/httpfs.duckdb_extension.gz --output $PWD/extensions/httpfs.duckdb_extension.gz
+curl https://extensions.duckdb.org/v1.3.1/linux_amd64/iceberg.duckdb_extension.gz --output $PWD/extensions/iceberg.duckdb_extension.gz
+curl https://extensions.duckdb.org/v1.3.1/linux_amd64/ducklake.duckdb_extension.gz --output $PWD/extensions/ducklake.duckdb_extension.gz
+curl https://community-extensions.duckdb.org/v1.3.1/linux_amd64/nanoarrow.duckdb_extension.gz -o $PWD/extensions/nanoarrow.duckdb_extension.gz
 
 # Unzip 
 gunzip $PWD/extensions/httpfs.duckdb_extension.gz
 gunzip $PWD/extensions/iceberg.duckdb_extension.gz
+gunzip $PWD/extensions/ducklake.duckdb_extension.gz
 gunzip $PWD/extensions/nanoarrow.duckdb_extension.gz
